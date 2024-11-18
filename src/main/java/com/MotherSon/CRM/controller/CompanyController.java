3ba53a22955f6d530433975eb13980abe12e72ac
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2024-11-19 00:00:19 +0530

    company and cors

--- src/main/java/com/MotherSon/CRM/controller/CompanyController.java
+++ src/main/java/com/MotherSon/CRM/controller/CompanyController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.MotherSon.CRM.security.services.CompanyService;
 import jakarta.persistence.EntityNotFoundException;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("Motherson/crm/v1/company")
 public class CompanyController {
     
